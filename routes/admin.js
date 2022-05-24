34e587ef0026347feb24cd28e9e0004baa60a7b0
Jonas <jonasericsson90@hotmail.com>
2022-05-24 20:45:37 +0200

    created const loginAdmin

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,24 @@ var router = express.Router();
 
 /* GET Admin */
 router.get("/", function (req, res, next) {
-  
-     res.send(
-       "<h1>Hej från admin</h1> <label>Admin</label> <br> <input id='admin' name='admin' type='text'> <br> <label>Password</label><br><input id='password' name='password' type='password'> <button>Logga in</button>"
-     );
+  const loginAdmin = `
 
-     if(req.body)
 
-     var admin = Document.getElementById('admin').value
-     console.log(admin);
+        <h1>Hej från admin</h1> 
+        
+        <label>Admin</label> <br>
+        <input id='admin' name='admin' type='text'> <br>
+        <label>Password</label><br>
+        <input id='password' name='password' type='password'> <br>
+        <button onClick= ${login()} >Logga in</button>
+        `;
 
+  res.send(loginAdmin);
 });
+
+function login() {
+  console.log("hej");
+}
 
 // router.get("/admin", function (req, res) {
 // res.sendFile("public/admin.html", { root: __dirname });
