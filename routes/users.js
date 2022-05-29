4df9a4cfdd916d9bf25437b527206e2b5f67fbc0
Jonas <jonasericsson90@hotmail.com>
2022-05-29 22:56:00 +0200

    bugg fix

--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,45 @@
-var express = require('express');
-const { json } = require('express/lib/response');
+var express = require("express");
+const { json } = require("express/lib/response");
 var router = express.Router();
 
-
 // Får användare från datorbas
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  req.app.locals.db
+    .collection("users")
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+      let printUsers = "<div><h2>Våra users</h2>";
 
-  req.app.locals.db.collection('users').find().toArray()
-  .then(results =>{
-    console.log(results);
-    let printUsers = '<div><h2>Våra users</h2>'
-
-    for (user in results){
-      printUsers += '<div>' + results[user].username + '</div>'
-    }
-    printUsers += "</div>"
-    res.send(printUsers);
-  })
+      for (user in results) {
+        printUsers += "<div>" + results[user].username + "</div>";
+      }
+      printUsers += "</div>";
+      res.send(printUsers);
+    });
 });
 
-
 // hämtar users till frontend
-router.post('/loggin', function(req, res, next) {
-
-  req.app.locals.db.collection('users').find().toArray()
-  .then(results =>{
-    console.log(results);
-  })
+router.post("/loggin", function (req, res, next) {
+  req.app.locals.db
+    .collection("users")
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+    });
 });
 
 // Pushar en ny användare
-router.post('/add', function(req,res){
-  
-  req.app.locals.db.collection('users').insertOne(req.body)
-  .then(result => {
-    console.log(result);
-    res.send('/');
-  })
+router.post("/add", function (req, res) {
+  req.app.locals.db
+    .collection("users")
+    .insertOne(req.body)
+    .then((result) => {
+      console.log(result);
+      res.send("/");
+    });
 });
 
 // router.get('/loggin', function (req, res, next){
@@ -44,7 +47,7 @@ router.post('/add', function(req,res){
 //   .then(results =>{
 
 //     let users = results
-    
+
 //     res.send(users);
 //   })
 // })
@@ -55,7 +58,7 @@ router.post('/add', function(req,res){
 
 //   if (obj.username == "admin" && obj.password == "admin") {
 //     console.log("OOK");
-    
+
 //   } else {
 //     console.log("Inte OK");
 //   }
